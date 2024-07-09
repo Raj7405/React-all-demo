351fc82d7b46579b3010cfae37eb056fc588748b
Raj7405 <rajendra.m.chaudhari2001@gmail.com>
2024-07-09 17:46:35 +0530

    intial setup for todo app using redux

---
+++ Redux-Tutorial/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <nav>
+        <section>
+          <h1>Redux Fundamentals Example</h1>
+          <div className="navContent">
+            <div className="navLinks"></div>
+          </div>
+        </section>
+      </nav>
+      <section>
+        <h2>Welcome to the Redux Fundamentals example app!</h2>
+      </section>
+    </div>
+  )
+}
+
+export default App
