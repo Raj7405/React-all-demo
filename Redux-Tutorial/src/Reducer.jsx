900b58f721e2e9d59301fab1f2aa7f8a6915c993
Raj7405 <rajendra.m.chaudhari2001@gmail.com>
2024-07-10 11:25:34 +0530

    Reducer composition without redux utility

--- Redux-Tutorial/src/Reducer.jsx
+++ Redux-Tutorial/src/Reducer.jsx
@@ -1,59 +1,11 @@
-const intialState = {
-  todos: [
-    { id: 0, text: "Learn React", completed: true },
-    { id: 1, text: "Learn Redux", completed: false, color: "purple" },
-    { id: 2, text: "Build something fun!", completed: false, color: "blue" },
-  ],
-  filters: {
-    status: "All",
-    colors: [],
-  },
-};
+import { act } from "react";
+import filtersReducer from "./feature/filters/filterSlice";
+import todosReducer from "./feature/todos/todosSlice";
 
-function nextTodoId(todos) {
-    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-    return maxId + 1
-}
 
-export default appReducer = (state = intialState, action) => {
-  switch (action.type) {
-    case "todos/todoAdded": {
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          {
-            id: nextTodoId(state.todos),
-            text: action.payload,
-            completed: false,
-          },
-        ],
-      };
+export default rootReducer = (state = {}, action) => {
+    return {
+        todos: todosReducer(state.todos,action),
+        filters: filtersReducer(state.filters, action),
     }
-    case 'todos/todoToggled': {
-      return {
-        ...state,
-        todos: state.todos.map((todos) => {
-          if (todos.id !== action.payload) {
-            return todos;
-          }
-          return {
-            ...todos,
-            completed: !todos.completed,
-          };
-        }),
-      };
-    }
-    case  'todos/statusFilterChanged': {
-        return {
-            ...state,
-            filters:{
-                ...state.filters,
-                status: action.payload,
-            }
-        }
-    }
-    default:
-      return state;
-  }
 };
