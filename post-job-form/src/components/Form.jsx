3748df8982f4461576d808db5893dea52dd6c866
Raj7405 <rajendra.m.chaudhari2001@gmail.com>
2024-07-15 18:45:57 +0530

    switch layout base on step

--- post-job-form/src/components/Form.jsx
+++ post-job-form/src/components/Form.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import JobDescription from './formSteps/JobDescription'
+import SkillForm from './formSteps/SkillForm'
+import ContractType from './formSteps/ContractType'
+import RecruitmentInfo from './formSteps/RecruitmentInfo'
+import GenralView from './formSteps/GenralView'
+import { useEffect } from 'react'
+import {useSelector} from 'react-redux'
+import {Outlet} from 'react-router'
 
-function Form() {
-  return (
-    <div>Form</div>
-  )
+function Form({currentStep, setCurrentStep}) {
+    const currStep = useSelector(state => state.currStep)
+    useEffect(() => {
+        console.log(currStep)
+    }   
+    ,[currStep])
+    console.log('re-render')
+
+    switch(currStep){
+        case 1: return <JobDescription/>
+        case 2: return <SkillForm/> 
+        case 3: return <ContractType/>
+        case 4: return <RecruitmentInfo/>
+        case 5: return <GenralView/>
+    }
 }
 
 export default Form
