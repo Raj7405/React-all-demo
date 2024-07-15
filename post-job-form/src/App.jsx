97f257e659c7707050b40360ce7a6cb7f1dc8fac
Raj7405 <rajendra.m.chaudhari2001@gmail.com>
2024-07-15 11:07:47 +0530

    Initial commit

---
+++ post-job-form/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'                
+import './App.css'
+import Form from './components/Form'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <Form/>
+    </>
+  )
+}
+
+export default App
